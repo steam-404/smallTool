de385cb16a0efaa2f064908b5f4e2a4fd57552c9
steam-404 <2507138657@qq.com>
2023-04-17 17:48:25 +0800

    微调一下布局

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,37 +1,75 @@
 <script>
-
+export default {
+    data() {
+        return {
+            span: 'auto'
+        }
+    }
+}
 </script>
 <template>
     <div id="main">
-        <el-container>
-            <el-aside>
-                <router-link to="/steam_404"><p>steam-404</p></router-link>
-                <router-link to="/linuo"><p>linuo</p></router-link>
-                <router-link to="/QQMessage"><p>QQ信息查询</p></router-link>
-                <router-link to="/QQGrade"><p>QQ等级加速</p></router-link>
-                <router-link to="/QQGame"><p>QQ游戏加速</p></router-link>
-                <router-link to="/QQDialogue"><p>QQ强制对话</p></router-link>
-                <router-link to="/QQAddGroup"><p>QQ强制加群</p></router-link>
-                <router-link to="/visitorMessage"><p>获取访客信息</p></router-link>
-                <router-link to="/ICP"><p>ICP备案查询</p></router-link>
-                <router-link to="/websiteTitle"><p>获取网站标题</p></router-link>
-                <router-link to="/websiteLogo"><p>获取网站logo</p></router-link>
-                <router-link to="/QrCodeAnalysis"><p>二维码解析</p></router-link>
-                <router-link to="/QrCodeGenerate"><p>二维码生成</p></router-link>
-                <router-link to="/baiduRecord"><p>百度收录查询</p></router-link>
-                <router-link to="/RandomComment"><p>网易云随机热门评论</p></router-link>
-                <router-link to="/RandomSong"><p>网易云随机歌曲</p></router-link>
-                <router-link to="/PortraitScreenImages"><p>随机竖屏图像</p></router-link>
-                <router-link to="/LandscapeScreenImages"><p>随机横屏图像</p></router-link>
-                <router-link to="/RandomHeadImages"><p>随机头像</p></router-link>
-            </el-aside>
-            <el-main>
-                <router-view></router-view>
-            </el-main>
-        </el-container>
+        <el-row :gutter="20">
+            <router-link to="/steam_404">steam-404</router-link>
+            <router-link to="/linuo">linuo</router-link>
+            <br>
+            <el-col v-model:span="span">
+                <router-link to="/QQMessage">QQ信息查询</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QQGrade">QQ等级加速</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QQGame">QQ游戏加速</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QQDialogue">QQ强制对话</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QQAddGroup">QQ强制加群</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/visitorMessage">获取访客信息</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/ICP">ICP备案查询</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/websiteTitle">获取网站标题</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/websiteLogo">获取网站logo</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QrCodeAnalysis">二维码解析</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/QrCodeGenerate">二维码生成</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/baiduRecord">百度收录查询</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/RandomComment">网易云随机热门评论</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/RandomSong">网易云随机歌曲</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/PortraitScreenImages">随机竖屏图像</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/LandscapeScreenImages">随机横屏图像</router-link>
+            </el-col>
+            <el-col v-model:span="span">
+                <router-link to="/RandomHeadImages">随机头像</router-link>
+            </el-col>
+        </el-row>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
-<style>
+<style scoped>
 #main p {
     color: green;
 }
